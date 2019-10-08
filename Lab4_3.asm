82f63a9bbd043559c60369396f52eca4ec41e2fb
josue0ghost <36039766+josue0ghost@users.noreply.github.com>
2019-10-08 11:50:01 -0600

    Update Lab4_3.asm

--- Lab4_3.asm
+++ Lab4_3.asm
@@ -8,6 +8,10 @@
     multi DB 'Multiplicacion: $'
     divi DB 'Division: $'
     resi DB 'Residuo: $'
+    
+    diez DB 10d
+    residuo DB ?
+    cociente DB ?
 .stack
 .code
 program:
@@ -43,12 +47,21 @@ program:
     mov num2, al
     sub num2, 30h
     
+    xor cx, cx
     mov cl, num2            ;contador  = multiplicador
     xor bx, bx
     
-    mult:
+    print_mult:
     add bl, num1            ; sumas sucesivas
-    loop mult
+    dec cx
+    jnz print_mult
+    
+    ;separo los dijitos
+    xor ax, ax
+    mov al, bl
+    div diez
+    mov residuo, al         ; guardo residuo
+    mov cociente, ah        ; guardo cociente
     
     mov dl, 0dh
     mov ah, 2h
@@ -60,23 +73,26 @@ program:
     mov ah, 09h
     int 21h
     
-    add bl, 30h             ;impresion de resutado
-    mov dl, bl
-    mov ah, 2h
+    add residuo, 30h             ;impresion de resutado
+    add cociente, 30h
+    mov dl, residuo
+    mov ah, 02h
     int 21h     
-    
+    mov dl, cociente
+    mov ah, 02h
+    int 21h     
     
     xor bx, bx
     mov bl, num1            ;iniciamos bl con num1
     
-    division:
+    print_division:
     cmp bl, 0h
     jle imprime             ;si es menor que 0 imprime cociente y residuo
     
     sub bl, num2            ;restas sucesivas
     inc bh                  ; en bl se guarda el residuo y en bh el cociente
     
-    jmp division
+    jmp print_division
     
     imprime:
     mov dl, 0dh
@@ -89,12 +105,12 @@ program:
     mov ah, 09h
     int 21h
     
-    add bh, 30h             ;impresion de resutado
+    add bh, 30h             ;impresion de resultado
     mov dl, bh
     mov ah, 2h
     int 21h
     
-    residuo:
+    print_residuo:
     mov dl, 0dh
     mov ah, 2h
     int 21h
